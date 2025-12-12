d98444ee379149a5a21ed37209f39645681ffdd7
Harinderpal singh <hsbhular@gmail.com>
2025-12-12 12:47:03 +0530

    y

--- obfuscate.js
+++ obfuscate.js
@@ -1,7 +1,11 @@
 // obfuscate.js
-const fs = require('fs');
-const path = require('path');
-const JavaScriptObfuscator = require('javascript-obfuscator');
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import JavaScriptObfuscator from 'javascript-obfuscator';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 console.log('🚀 Starting obfuscation process...');
 
--- src/App.js
+++ src/App.js
@@ -4,21 +4,12 @@ import { Box } from "@mui/material";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-<<<<<<< HEAD
-
 import YouTubeDownloader from "./components/YouTubeDownloader";
 import InstagramDownloader from "./components/InstagramDownloader";
 import logo from "./components/assets/logo.png";
 import CookieConsent from "./components/CookieConsent";
 import Privacy from "./components/Privacy";
 import About from "./components/About";
-=======
-import AboutPage from "./components/pages/AboutPage";
-import YouTubeDownloader from "./components/YouTubeDownloader";
-import logo from "./components/assets/logo.png";
->>>>>>> 0035b5a58621603643ba2ef4cd93b52418e875c0
-
-
 import MaintenancePage from "./components/pages/MaintenancePage";
 
 import Lottie from "lottie-react";
@@ -28,13 +19,7 @@ import axios from "axios";
 import loaderAnim from "./components/assets/loader.json";
 
 // Pages
-
 import Mp3Page from "./components/pages/Mp3Page";
-<<<<<<< HEAD
-  
-=======
-import InstagramPage from "./components/pages/InstagramPage";
->>>>>>> 0035b5a58621603643ba2ef4cd93b52418e875c0
 import ReelsPage from "./components/pages/ReelsPage";
 import FacebookPage from "./components/pages/FacebookPage";
 
@@ -53,12 +38,8 @@ function App() {
 
   try {
     // ✅ Send the URL in JSON body, not as params
-<<<<<<< HEAD
-      const API_BASE = process.env.REACT_APP_API_URL || "http://127.0.0.1:8000";
-      const res = await axios.post(`${API_BASE}/info`, { url });
-=======
-    const res = await axios.post("https://api.savefrom.in/api/info", { url });
->>>>>>> 0035b5a58621603643ba2ef4cd93b52418e875c0
+    const API_BASE = process.env.REACT_APP_API_URL || "http://127.0.0.1:8000";
+    const res = await axios.post(`${API_BASE}/info`, { url });
 
     if (res.data && res.data.formats) {
       setVideoData({ ...res.data, url });
@@ -90,38 +71,19 @@ function App() {
         <Routes>
           {/* Main working downloader page */}
           <Route path="/" element={<YouTubeDownloader />} />
-<<<<<<< HEAD
           <Route path="/instagram" element={<InstagramDownloader />} />
-=======
->>>>>>> 0035b5a58621603643ba2ef4cd93b52418e875c0
-
-          {/* Maintenance pages (stylish spinner) */}
           <Route path="/Mp3Page" element={<Mp3Page />} />
-                  <Route path="/instagram" element={<MaintenancePage />} />
-            <Route path="/facebook" element={<MaintenancePage />} />
-<<<<<<< HEAD
+          <Route path="/facebook" element={<MaintenancePage />} />
           <Route path="/about" element={<About />} />
-=======
-          <Route path="/about" element={<MaintenancePage />} />
->>>>>>> 0035b5a58621603643ba2ef4cd93b52418e875c0
-
-          {/* Optional pages (if you want separate ones later) */}
-          <Route path="/Mp3Page" element={<Mp3Page />} />
           <Route path="/reels" element={<ReelsPage />} />
           <Route path="/fb" element={<FacebookPage />} />
-<<<<<<< HEAD
           <Route path="/privacy" element={<Privacy />} />
-=======
->>>>>>> 0035b5a58621603643ba2ef4cd93b52418e875c0
         </Routes>
 
         {/* ✅ Footer at bottom */}
         <Footer />
+        <CookieConsent />
       </Box>
-<<<<<<< HEAD
-      <CookieConsent />
-=======
->>>>>>> 0035b5a58621603643ba2ef4cd93b52418e875c0
     </Router>
   );
 }
