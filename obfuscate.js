3c06a4ad7f8c3396fabe6f2893d2cff72d541bd1
Harinderpal singh <hsbhular@gmail.com>
2025-12-13 07:08:32 +0530

    hi

--- obfuscate.js
+++ obfuscate.js
@@ -1,11 +1,7 @@
 // obfuscate.js
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import JavaScriptObfuscator from 'javascript-obfuscator';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const fs = require('fs');
+const path = require('path');
+const JavaScriptObfuscator = require('javascript-obfuscator');
 
 console.log('🚀 Starting obfuscation process...');
 
